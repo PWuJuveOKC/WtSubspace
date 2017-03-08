eb1ec82dc97d0892e63bd1f9407e366c21d9351b
PWuJuveOKC <pwusff@gmail.com>
2017-03-08 17:42:47 -0500

    first commit

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ except ImportError:
 setup(
       name = 'WtSubspace',
       packages = ['WtSubspace'], # this must be the same as the name above
-      version = '0.1'
+      version = '0.1',
       description = 'Weighted random subspace method for classification',
       author = 'Peng Wu',
       license='LICENSE.txt',
       author_email = 'pwusff@gmail.com',
-      long_description=open('README.md').read(),
+      download_url = 'https://github.com/PWuJuveOKC/WtSubspace',
+      keywords = ['Random Subspace', 'Weighted Method', 'Classification'],
       install_requires=["scikit-learn >= 0.17", "pandas >= 0.17.1"]
       )
